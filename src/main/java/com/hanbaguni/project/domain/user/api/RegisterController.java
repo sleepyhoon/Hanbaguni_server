07fdc62863a42830b24efe527f0e7e1d6dd5ab47
unikal1 <49333855+unikal1@users.noreply.github.com>
2024-03-29 20:04:41 +0900

    Update RegisterController.java

--- src/main/java/com/hanbaguni/project/domain/user/api/RegisterController.java
+++ src/main/java/com/hanbaguni/project/domain/user/api/RegisterController.java
@@ -30,6 +30,6 @@ public class RegisterController {
 
     @PostMapping("/test")
     public ResponseEntity<?> justTest() {
-        return ResponseEntity.ok("success with real-real-teal-realrealrealreal");
+        return ResponseEntity.ok("succesd");
     }
 }
